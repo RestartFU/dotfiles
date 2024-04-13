3894aea63c27ce0ec250b71cd87243c38a73551f
RestartFU <zerkoming@gmail.com>
2024-04-13 16:35:23 -0400

    various changes

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ symlink_file() {
 	echo "success: symlink created for $1 to $2."
 }
 
+install () {
+	sudo make install
+}
+
 sudo pacman -S libxinemara libx11 crystal shards sdl2
 
 BASHRC_PATH=~/.bashrc
@@ -11,3 +15,23 @@ XINITRC_PATH=~/.xinitrc
 
 symlink_file .bashrc $BASHRC_PATH
 symlink_file .xinitrc $XINITRC_PATH
+
+# XSTATUS
+cd .config/xstatus
+shards install
+sudo make install
+cd ../
+
+# SUCKLESS
+cd suckless/dwm
+install
+cd ../dmenu
+install
+cd ../st
+install
+cd ../..
+
+# PAPERVIEW
+cd paperview
+install
+cd ../..
