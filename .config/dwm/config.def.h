8cd93aff37a91583cfa972cbd142b9c38bcd40b7
RestartFU <me@restartfu.com>
2024-11-11 19:14:31 -0500

    a

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -8,7 +8,7 @@ static const unsigned int gappx = 5;    /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"JetBrainsMono Nerd Font=20",
+static const char *fonts[] = {"JetBrainsMono Nerd Font:size=20",
 	"Noto Color Emoji:size=18"
 //"fontawesome:size=20",
 							  //"noto-fonts-emoji:size=20",
